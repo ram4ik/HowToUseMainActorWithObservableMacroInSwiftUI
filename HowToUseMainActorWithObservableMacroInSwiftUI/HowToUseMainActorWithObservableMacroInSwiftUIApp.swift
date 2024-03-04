24b35d37ab073d3c1479441eeafa102fee71b317
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-03-04 20:01:28 +0200

    Initial Commit

---
+++ HowToUseMainActorWithObservableMacroInSwiftUI/HowToUseMainActorWithObservableMacroInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseMainActorWithObservableMacroInSwiftUIApp.swift
+//  HowToUseMainActorWithObservableMacroInSwiftUI
+//
+//  Created by test on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseMainActorWithObservableMacroInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
